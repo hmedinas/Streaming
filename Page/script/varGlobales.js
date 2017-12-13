62d56e2a20d66ba54a0273d754ea11c9d5499fb0
Jopau3112 <jopau3112@gmail.com>
2017-12-13 13:43:26 +0100

    globales

--- Page/script/varGlobales.js
+++ Page/script/varGlobales.js
@@ -1,3 +1,9 @@
+//MADRID
 var idMaquina = "1234";
 var origen = "Madrid";
 var destino = "Barcelona";
+
+// //BARCELONA
+// var idMaquina = "5678";
+// var origen = "Barcelona";
+// var destino = "Madrid";
