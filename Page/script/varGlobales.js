b8d3094f5e078a48315783036436822274a4d1d0
Jopau3112 <jopau3112@gmail.com>
2017-12-18 11:49:03 +0100

    zoom añadido

--- Page/script/varGlobales.js
+++ Page/script/varGlobales.js
@@ -1,9 +1,17 @@
-//MADRID
-var idMaquina = "1234";
-var origen = "Madrid";
-var destino = "Barcelona";
-
-// //BARCELONA
-// var idMaquina = "5678";
-// var origen = "Barcelona";
-// var destino = "Madrid";
+if (localStorage.getItem('LS_Codigo')) {
+    var codigo = JSON.parse(localStorage.getItem('LS_Codigo'));
+    switch (codigo) {
+        case "M":
+            //MADRID
+            var idMaquina = "1234";
+            var origen = "Madrid";
+            var destino = "Barcelona";
+            break;
+        case "B":
+            //BARCELONA
+            var idMaquina = "5678";
+            var origen = "Barcelona";
+            var destino = "Madrid";
+            break;
+    }
+}
